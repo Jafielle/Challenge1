53261a5cbd9edace8f5795325eb3b99a4ddccc9e
Jafielle <abrilgomez.aray@gmail.com>
2023-02-01 17:13:50 -0500

    dark mode modified to alter section2 / footer color changed & icons transition added

--- darkMode.js
+++ darkMode.js
@@ -3,6 +3,11 @@ const inputSwitch = document.querySelector(".input")
 const bodyPage = document.querySelector("body")
 console.log(inputSwitch.checked);
 
+const section2Page = document.querySelector(".section2")
+
+const h2Page1 = document.querySelector(".title1")
+
+const h2Page2 = document.querySelector(".title2")
 
 
 inputSwitch.checked = JSON.parse(localStorage.getItem("mode"));
@@ -11,10 +16,18 @@ updateMode();
 
 function updateMode() {
       if (inputSwitch.checked) {
-            bodyPage.style.background = "black"
+            bodyPage.style.background = "black";
+            section2Page.style.background = "#343A40";
+            h2Page1.style.color = "white";
+            section2Page.style.color = "#AAB2D5";
+            h2Page2.style.color = "#AAB2D5";
       }
       else {
-            bodyPage.style.background = "#EFF1FA"
+            bodyPage.style.background = "#EFF1FA";
+            section2Page.style.background = "white";
+            h2Page1.style.color = "#343A40";
+            section2Page.style.color = "#495057";
+            h2Page2.style.color = "343A40";
       }
 }
 
